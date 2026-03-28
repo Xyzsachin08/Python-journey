e3a7929bb5a8b0bb5b63797b0ceb90b7752e91ee
Sachin Borude <sachinborude0610@gmail.com>
2026-03-28 11:28:50 +0530

    Simple Interest Calculator

---
+++ Advance Python Learning journey/Concept_01/Simple_Interest_Calculator.py
@@ -0,0 +1,10 @@
+"Take principal, rate, and time and calculate interest."
+
+
+p = float(input("Enter a pricipal amount: "))
+r = float(input("Enter a rate of interest: "))
+t = float(input("Enter a time: "))
+
+si = (p * r * t) / 100
+
+print("Simple interest is ", si)

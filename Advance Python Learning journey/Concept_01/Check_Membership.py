e8c843d292712db446e0bad8439445a4c1ac5b9e
Sachin Borude <sachinborude0610@gmail.com>
2026-03-28 11:28:28 +0530

    Check Membership

---
+++ Advance Python Learning journey/Concept_01/Check_Membership.py
@@ -0,0 +1,8 @@
+"Ask user for a character and check if it exists in string python."
+
+c = input("Enter a character: ")
+
+if c in "python":
+    print("Character exists in 'python'")
+else:
+    print("Character does not exist in 'python'")
